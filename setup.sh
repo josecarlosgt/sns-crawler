6b15967b6785fc1a941f851228928ccf2b596fc2
Jose Carlos <charlygt@gmail.com>
2016-06-22 09:46:50 +1200

    Adding setup scripts

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+# Application
+
+# mkdir -p ~/research
+# cd ~/research
+# git clone https://github.com/josecarlosgt/sns-crawler.git
+
 # Logging
 
 # Create log directory
 sudo mkdir -p /var/log/sns-crawler/
-
-# Application
-
-mkdir -p ~/research
-cd ~/research
-git clone https://github.com/josecarlosgt/sns-crawler.git
